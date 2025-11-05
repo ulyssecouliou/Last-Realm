e698f986116262872ff00a46512e3a2667182e52
Titouanalltoo <hillion.titouan@gmail.com>
2025-11-05 15:02:48 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,8 +42,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
+// 404 handler - must be last
+app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
